5ad98ab99ba960d1b240e8037ce18960cd030822
Juma Harun <mmbh4run@gmail.com>
2023-04-01 21:25:54 +0300

    markup and styles for ProductDesc

--- src/components/ProductComponents/ProductDesc.jsx
+++ src/components/ProductComponents/ProductDesc.jsx
@@ -3,10 +3,29 @@ import productDetails from '../../productDetails.json';
 function ProductDesc() {
     const { company, title, description, price, discount, discountedPrice } =
         productDetails[0];
+
+    const getCurrencyLocale = (str) => {
+        return Number(str).toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD',
+        });
+    };
+
     return (
-        <section className="product-desc">
-            <p>{company}</p>
-            <h1 className="bold">{title}</h1>
+        <section className="product-desc pall-1">
+            <div className="container">
+                <p className="company clr-orange">{company}</p>
+                <h1 className="bold">{title}</h1>
+                <p className="desc">{description}</p>
+
+                <div className="price-wrapper flex">
+                    <h2 className="discounted-price">
+                        {getCurrencyLocale(discountedPrice)}
+                    </h2>
+                    <span className="discount clr-orange"> {discount}%</span>
+                    <p className="price">{getCurrencyLocale(price)}</p>
+                </div>
+            </div>
         </section>
     );
 }
